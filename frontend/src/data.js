999440d07b9bd2e1f6aa21f27bc72cbdea375128
nanipreetham <18btrcs017@jainuniversity.ac.in>
2021-04-23 17:26:44 +0530

    initial

---
+++ frontend/src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name: 'Nike slim shirt',
+            category: 'shirts',
+            image: './images/p1.png',
+            price: 120,
+            brand: 'Nike',
+            rating: '4.5',
+            numReviews: '13',
+            description:' high quality product',
+
+        },
+        {
+            _id:'2',
+            name: 'Polo fit shirt',
+            category: 'shirts',
+            image: './images/p2.png',
+            price: 150,
+            brand: 'Polo',
+            rating: '4.7',
+            numReviews: '15',
+            description:' high quality product',
+
+        },
+        {
+            _id:'3',
+            name: 'Wrangler jeans pant',
+            category: 'pantss',
+            image: './images/p3.png',
+            price: 220,
+            brand: 'Wrangler',
+            rating: '4.0',
+            numReviews: '33',
+            description:' high quality product',
+
+        },
+        {
+            _id:'4',
+            name: 'Jean shirt',
+            category: 'shirts',
+            image: './images/p4.png',
+            price: 500,
+            brand: 'Nike',
+            rating: '4.9',
+            numReviews: '23',
+            description:' high quality product',
+
+        },
+        {
+            _id:'5',
+            name: 'Eagle formal pant',
+            category: 'pants',
+            image: './images/p5.png',
+            price: 290,
+            brand: 'Eagle',
+            rating: '4.5',
+            numReviews: '37',
+            description:' high quality product',
+
+        },
+    ],
+};
+
+export default data;
